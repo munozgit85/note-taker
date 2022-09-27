27b31585104f86bd0b7346cc8d7217485d2a996e
Racquel Munoz <racquelg85@hotmail.com>
2022-09-27 11:35:53 -0500

    add notes.js buttons

--- routes/apiRoutes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const indexRoutes = require('../apiRoutes/indexRoutes');
-
-router.use(indexRoutes);
-
-module.exports = router;
-
-router.use(require('./notesRoutes'));
--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { db } = require('./db/db');
 
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const notesRoutes = require('./routes/notes');
 
 
 
 
-
-app.use(express.static(__dirname));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
+app.use(express.static(__dirname));
+app.use('/api', notesRoutes);
 
 
 
